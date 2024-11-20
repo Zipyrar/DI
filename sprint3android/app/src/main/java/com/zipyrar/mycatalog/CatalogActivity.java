c1f0f97b1ec5b3c3d03ed210cf050ef1244daf67
Zipyrar <agarciac23@fpcoruna.afundacion.org>
2024-11-20 10:24:39 +0100

    Creado el botón de navegar.

--- sprint3android/app/src/main/java/com/zipyrar/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/zipyrar/mycatalog/CatalogActivity.java
@@ -1,6 +1,11 @@
 package com.zipyrar.mycatalog;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.PixelCopy;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
 public class CatalogActivity extends AppCompatActivity {
+    private Button navigate;
+    private Context context;
+    private RequestQueue request;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_catalog);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        navigate = findViewById(R.id.navigateDetail);
+
+        context = this;
+
+        navigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
         });
+
+        request = Volley.newRequestQueue(this);
     }
 }
