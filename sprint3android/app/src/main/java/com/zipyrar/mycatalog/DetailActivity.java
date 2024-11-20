80c882273dc12266b6187de448f09d9fee8f5489
Zipyrar <agarciac23@fpcoruna.afundacion.org>
2024-11-20 20:14:05 +0100

    Añadidos textos, imágen, y otro botón.

--- sprint3android/app/src/main/java/com/zipyrar/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/zipyrar/mycatalog/DetailActivity.java
@@ -1,12 +1,18 @@
 package com.zipyrar.mycatalog;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.time.Instant;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -20,5 +26,12 @@ public class DetailActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageView imageView = findViewById(R.id.image);
+
+        Glide.with(this)
+                .load(R.drawable.aguila) // Reemplaza con tu recurso o URL de imagen
+                .apply(RequestOptions.circleCropTransform()) // Redondear la imagen
+                .into(imageView);
     }
 }
