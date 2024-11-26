df6a5a4e3c2de365c286e775616663222c892933
Zipyrar <agarciac23@fpcoruna.afundacion.org>
2024-11-26 13:55:57 +0100

    Añadido el acceso al DetailActivity.

--- sprint3android/app/src/main/java/com/zipyrar/mycatalog/CatalogFragment.java
+++ sprint3android/app/src/main/java/com/zipyrar/mycatalog/CatalogFragment.java
@@ -1,16 +1,20 @@
 package com.zipyrar.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class CatalogFragment extends Fragment {
-    
+
 
     public CatalogFragment() {
         // Required empty public constructor
@@ -24,14 +28,25 @@ public class CatalogFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
         }
     }
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_catalog, container, false);
+        View view = inflater.inflate(R.layout.fragment_catalog, container, false);
+
+        Button button = view.findViewById(R.id.navigateDetail);
+
+        button.setOnClickListener(v-> {
+            Intent intent = new Intent(requireContext(), DetailActivity.class);
+            startActivity(intent);
+                }
+        );
+
+        return view;
     }
 }
