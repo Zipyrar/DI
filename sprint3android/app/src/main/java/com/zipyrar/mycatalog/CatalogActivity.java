2ffefb4d7fcb783ad4e9ec13f7577671366449d7
Zipyrar <agarciac23@fpcoruna.afundacion.org>
2024-11-26 13:00:03 +0100

    Añadidos fragments, y el menú.

--- sprint3android/app/src/main/java/com/zipyrar/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/zipyrar/mycatalog/CatalogActivity.java
@@ -3,15 +3,10 @@ package com.zipyrar.mycatalog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.PixelCopy;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
