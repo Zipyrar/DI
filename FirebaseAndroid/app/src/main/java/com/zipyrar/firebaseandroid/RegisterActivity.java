93b17f737990e06b2acde706bdb35a4fa3fb4cf8
Zipyrar <agarciac23@fpcoruna.afundacion.org>
2025-01-16 20:32:04 +0100

    Parte Java del registro para guardar el correo y la contraseña.

--- FirebaseAndroid/app/src/main/java/com/zipyrar/firebaseandroid/RegisterActivity.java
+++ FirebaseAndroid/app/src/main/java/com/zipyrar/firebaseandroid/RegisterActivity.java
@@ -1,7 +1,37 @@
 package com.zipyrar.firebaseandroid;
 
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
 
+public class RegisterActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.botRegistro).setOnClickListener(v -> registerUser());
+    }
+
+    private void registerUser() {
+        String email = ((EditText) findViewById(R.id.emailRegistro)).getText().toString();
+        String password = ((EditText) findViewById(R.id.contraRegistro)).getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(RegisterActivity.this, "Usuario registrado correctamente.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegisterActivity.this, "Error en el registro: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
 }
